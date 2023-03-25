b84ca4c02fac5b7b33563b90f92ba2f938c55555
Phillip <anhht@bfast.dev>
2023-03-25 10:11:26 +0700

    handle logout

--- src/screens/layout/Layout.js
+++ src/screens/layout/Layout.js
@@ -3,8 +3,10 @@ import {
   DesktopOutlined,
   ContainerOutlined,
   PieChartOutlined,
+  UserOutlined,
 } from '@ant-design/icons';
-import { Menu } from 'antd';
+import { Menu, Select } from 'antd';
+import React from "react";
 import './Layout.scss'
 import logo from '../../assets/images/logo.png'
 function getItem(label, key, icon, tolink, children) {
@@ -24,6 +26,12 @@ const items = [
 
 const Layout = () => {
   const navigate = useNavigate();
+  const name = localStorage.getItem("name")
+  const onChange = (value) => {
+    // console.log(value)
+    localStorage.clear();
+    navigate('/auth/login')
+  }
   return (
     <div className="home-page">
       <div className="home-page-header">
@@ -44,6 +52,42 @@ const Layout = () => {
             }
           >
           </Menu>
+          <Select
+          className="nav-user-select"
+          showArrow={false}
+          placeholder={
+            <React.Fragment>
+              <div className="row" style={{height: 40}}>
+                <UserOutlined style={{marginRight: 10}}/>
+                {name}
+              </div>
+            </React.Fragment>
+          }
+          bordered={false}
+          labelInValue
+          dropdownStyle={{
+            borderRadius: 0,
+          }}
+          onChange={onChange}
+          options={[
+            // {
+            //   value: 'profile',
+            //   label: (
+            //     <div className="flex items-center border-gray1 border-b border-solid border-0 pb-3">
+            //       Profile
+            //     </div>
+            //   ),
+            // },
+            {
+              value: 'logout',
+              label: (
+                <div className="flex items-center border-b-stone-400">
+                  Logout
+                </div>
+              ),
+            },
+          ]}
+        />
         </div>
         <div className="layout-home">
           <Outlet />
