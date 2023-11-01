b86c21ebf8c13ecd2029ee1607ae0a549d77fbb8
Phillip <anhht@bfast.dev>
2023-11-01 10:14:28 +0700

    update Thanking page

--- src/screens/candidatetesting/thank/Thank.js
+++ src/screens/candidatetesting/thank/Thank.js
@@ -1,21 +1,17 @@
-import './thank.scss';
-import logo from '../../../assets/images/logo.png'
+import "./thank.scss";
+import logo from "../../../assets/images/logo3x.png";
 const Thank = () => {
-    return (
-        <div className='thank-screen '>
-            <div className='company-logo'>
-                <img src={logo} alt='company logo' />
-            </div>
-            <div className='thank-text'>
-                <div className='text-1'>
-                    Thanks for testing
-                </div>
-                <div className='text-2'>
-                    We will call back when complete result!
-                </div>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="thank-screen ">
+      <div className="company-logo">
+        <img src={logo} alt="company logo" />
+      </div>
+      <div className="thank-text">
+        <div className="text-1">Thanks for testing</div>
+        <div className="text-2">We will call back when complete result!</div>
+      </div>
+    </div>
+  );
+};
 
 export default Thank;
